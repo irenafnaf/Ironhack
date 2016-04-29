6ab17a0d016444f287684eca6123033ed7a7ebad
Irene Fernandez <irenafnaf@gmail.com>
2016-04-28 21:35:44 -0400

    Shake Shack Exercise with the shopping cart included (Checkout)

--- shake_shack/shake_shack.rb
+++ shake_shack/shake_shack.rb
@@ -1,3 +1,23 @@
+class ShakeShop
+	def initialize
+		@shakes = []
+	end
+
+	def add_shake(milkshake)
+		@shakes.push(milkshake)
+	end
+
+	def checkout
+		total = 0
+		@shakes.each do | single_shake |
+		total += single_shake.total_price
+		end
+		puts "Your price of the day was: #{total}, have a nice day!}"
+	end
+end
+
+
+
 class MilkShake
 	def initialize
 		@base_price = 3
@@ -37,10 +57,22 @@ chocolate_chips = Ingredient.new("Chocolate Chips", 1)
 caramel = Ingredient.new("Caramel", 1)
 # Menu
 
+# My Shake
 my_shake = MilkShake.new
 my_shake.add_ingredient(banana)
 my_shake.add_ingredient(caramel)
 
-puts my_shake.total_price
+#puts my_shake.total_price
+
+# Faraz's Shake
+farazs_shake = MilkShake.new
+farazs_shake.add_ingredient(berries)
+farazs_shake.add_ingredient(nuts)
+
+# My New Shop
+irenes_shop = ShakeShop.new
+irenes_shop.add_shake(my_shake)
+irenes_shop.add_shake(farazs_shake)
+irenes_shop.checkout
 
 
