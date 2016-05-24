b382ec416462a462b7d330bd728b5d12212174c6
Irene Fernandez <irenafnaf@gmail.com>
2016-05-24 18:18:17 -0400

    Don't remember

--- week_3/user_password_authenticator/app.rb
+++ week_3/user_password_authenticator/app.rb
@@ -1,6 +1,7 @@
 # app.rb
 require_relative("lib/authenticator.rb")
 require_relative('lib/word_stuff.rb')
+require_relative('lib/counter_picker.rb')
 
 auth = Authenticator.new("Josh", "swordfish")
 
@@ -19,19 +20,12 @@ if auth.verify(username_input, password_input) # Call upon Authenticator to veri
  	sentence_input = gets.chomp
 # Do word counter things
  	my_counter = WordStuff.new(sentence_input)
- 	puts "Do you want to: \n count words / \n count letters / \n reverse sentence / \n make them uppercase / \n make them lowercase"
+	puts "Do you want to: \n count words / \n count letters / \n reverse sentence / \n make them uppercase / \n make them lowercase"
  	answer = gets.chomp
- 		if answer == "count words"
-		 	puts "The number of words in your sentence was #{my_counter.counter_words}" 
-		 elsif answer == "count letters"
-			puts "The number of letters in your sentence was #{my_counter.counter_letters}"
- 		 elsif answer == "reverse sentence"
- 			puts "Your sentence in reverse looks like this: #{my_counter.reverse_sentence}"
- 		 elsif answer == "uppercase"
- 		 	puts "And in uppercase looks like this: #{my_counter.uppercase}"
- 		 elsif answer == "lowercase"
-			puts "And in lowercase like this: #{my_counter.lowercase}"
-		end
+ 	counter_picker = CounterPicker.new(answer, my_counter)
+ 	counter_picker.perform_operation
+
+
 
  else
 	puts "Your credentials are't valid. Try Again!"
