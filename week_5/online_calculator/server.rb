fa5e4009aee87dcdc07e5852360cd1ec3499696d
Irene Fernandez <irenafnaf@gmail.com>
2016-05-26 19:59:05 -0400

    modified it for all operations, added calculator instance to use calculator method

--- week_5/online_calculator/server.rb
+++ week_5/online_calculator/server.rb
@@ -1,4 +1,5 @@
 require "sinatra"
+require_relative "lib/calculator"
 
 get "/" do	
 	erb :home
@@ -21,10 +22,32 @@ get "/divide" do
 	erb :divide
 end
 
+calculator = Calculator.new
+
 post "/calculate_add" do
 	first = params[:first_number].to_f
 	second = params[:second_number].to_f
-	result = first + second
+	result = calculator.add(first, second)
 	"#{first} + #{second} = #{result}"
+end
 
+post "/calculate_subtract" do
+	first = params[:first_number].to_f
+	second = params[:second_number].to_f
+	result = calculator.subtract(first, second)
+	"#{first} - #{second} = #{result}"
+end
+
+post "/calculate_multiply" do
+	first = params[:first_number].to_f
+	second = params[:second_number].to_f
+	result = calculator.multiply(first, second)
+	"#{first} * #{second} = #{result}"
+end
+
+post "/calculate_divide" do
+	first = params[:first_number].to_f
+	second = params[:second_number].to_f
+	result = calculator.divide(first, second)
+	"#{first} / #{second} = #{result}"
 end
