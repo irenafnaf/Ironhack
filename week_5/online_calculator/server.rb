52ff63930fd63ebdfdfe3777a2a044e8b7f94354
Irene Fernandez <irenafnaf@gmail.com>
2016-05-28 09:57:13 -0400

    Created the post ofr the calculate using result view by writing a file and reading that file for result

--- week_5/online_calculator/server.rb
+++ week_5/online_calculator/server.rb
@@ -25,25 +25,34 @@ end
 calculator = Calculator.new
 
 post "/calculate" do
-	first = params[:first_number].to_f
-	second = params[:second_number].to_f
+	@first = params[:first_number].to_f
+	@second = params[:second_number].to_f
 	operation = params[:operation]
 		if operation == "addition"
-				result = calculator.add(first, second)
-				"#{first} + #{second} = #{result}"
+				@result = calculator.add(@first, @second)
+				@message = "#{@first} + #{@second} = #{@result}"
 
 		elsif operation == "subtraction"
-				result = calculator.subtract(first, second)
-				"#{first} - #{second} = #{result}"
+				@result = calculator.subtract(@first, @second)
+				@message = "#{@first} - #{@second} = #{@result}"
 
 		elsif operation == "multiplication"
-				result = calculator.multiply(first, second)
-				"#{first} * #{second} = #{result}"
+				@result = calculator.multiply(@first, @second)
+				@message = "#{@first} * #{@second} = #{@result}"
 
 		elsif operation == "division"
-				result = calculator.divide(first, second)
-				"#{first} / #{second} = #{result}"
+				@result = calculator.divide(@first, @second)
+				@message = "#{@first} / #{@second} = #{@result}"
 		end
+		erb :calculate
+end
+
+post "/calculate_using_result" do
+	@result = params[:result]
+	IO.write("public/results.txt", @result)
+
+	@first_num = IO.read("public/results.txt")
+	erb :calculate_using_result
 end
 
 # post "/calculate_subtract" do
