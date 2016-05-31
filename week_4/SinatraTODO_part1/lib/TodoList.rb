b623850e4bc0de93b63a0dd968a5a2e46eebf699
Irene Fernandez <irenafnaf@gmail.com>
2016-05-31 17:40:23 -0400

    Finished last iteration with the sort by created method

--- week_4/SinatraTODO_part1/lib/TodoList.rb
+++ week_4/SinatraTODO_part1/lib/TodoList.rb
@@ -1,19 +1,35 @@
 class TodoList
-    attr_reader :tasks
-    def initialize
-        @tasks = []
-    end
+   attr_reader :tasks, :user
 
-    def add_task(new_task)
-    	@tasks.push(new_task)
-    end
+   def initialize(user)
+       @user = user
+       @tasks = []
+   end
 
-    def delete_task(id)
-    	@tasks.delete_if do | task |
-    		task.id == id
-    	end
-    end
+   def add_task(task)
+       @tasks.push(task)
+   end
 
-    def find_task_by_id(id)
-    	
+   def delete_task(id)
+       @tasks.delete_if do | task |
+            task.id == id
+       end
+   end
+
+   def find_task_by_id(id)
+       task = @tasks.find do | task |
+           task.id == id
+       end
+   end
+
+   def sort_by_created(direction)
+       sorted_tasks = @tasks
+           
+           if direction == "ASC"
+               sorted_tasks.sort! { | task1, task2 | task1.id <=> task2.id }
+            elsif direction == "DESC"
+               sorted_tasks.sort! { | task1, task2 | task2.id <=> task1.id }
+           end
+       sorted_tasks
+   end
 end
