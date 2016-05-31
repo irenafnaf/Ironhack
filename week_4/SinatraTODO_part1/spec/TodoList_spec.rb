ff4b2d462bd8b4b82903fb637ed3304bdfa4632e
Irene Fernandez <irenafnaf@gmail.com>
2016-05-31 17:41:44 -0400

    Changed all the testing to match Mattheus

--- week_4/SinatraTODO_part1/spec/TodoList_spec.rb
+++ week_4/SinatraTODO_part1/spec/TodoList_spec.rb
@@ -1,35 +1,94 @@
-require_relative ("../lib/TodoList")
-require_relative ("../lib/sinatraTodo")
-require_relative "./spec_helper"
+require_relative "../lib/task.rb"
+require_relative "../lib/TodoList.rb"
+require "./spec_helper"
 
-describe "#TodoList" do 
-	before :each do
-		@todo_list = TodoList.new
-		@task = Task.new("Buy the Milk")
-		@task_2 = Task.new("Walk the dog")
-	end
+describe "Sinatra Todo" do
 
-	describe "#add_task" do
-		it "should add a task to the list" do
-			@todo_list.add_task(@task)
-			expect(@todo_list.tasks.length).to eq(1)
-		end
-	end
+    before :each do
+        @task1 = Task.new("Buy the milk")
+        @task2 = Task.new("Wash the car")
+        @task3 = Task.new("Make my todo list into a web app")
 
-	describe "#delete_task" do
-		it "should delete task by Id" do
-		@todo_list.add_task(@task)
-		@todo_list.delete_task(@task.id)
-		expect(@todo_list.tasks.length).to eq(0)
-		end
-	end
+        @todo_list = TodoList.new("Josh")
 
-	describe "#find_task_by_id" do
-		it "should find task by id" do
-		@todo_list.add_task(@task)
-		@todo_list.add_task(@task_2)
-		@todo_list.find_task_by_id(1)
-		expect(@todo_list.tasks.length).to eq()
+        @todo_list.add_task(@task1)
+        @todo_list.add_task(@task2)
+        
+    end
+
+    describe "Task" do
+        it "Show the increment in task id" do
+            expect(@task2.id).to eq(2)
+        end
+
+        it "Show the task was not completed" do
+            expect(@task1.complete?).to eq(false)
+        end
+
+        it "Show the completion status of task" do
+            @task2.complete!
+            expect(@task2.complete?).to eq(true)
+        end
+
+        it "Change the status from complete to incomplete" do
+            @task3.complete!
+            @task3.make_incomplete!
+            expect(@task3.complete?).to eq(false)
+        end
+
+        it "Update content in the task" do
+            @task1.update_content!("Buy the car")
+            expect(@task1.content).to eq("Buy the car")
+        end
+    end
+
+    describe "Todo List" do
+
+        it "Add new task to list" do
+            expect((@todo_list.tasks[0]).content).to eq("Buy the milk")
+        end
+
+        it "Delete task from list by task id" do
+            @todo_list.delete_task(19)
+            expect((@todo_list.tasks).length).to eq(1)
+        end
+
+        it "Find the task by task id" do
+            @task = @todo_list.find_task_by_id(@task1.id)
+            expect(@task.content).to eq("Buy the milk")
+        end
+
+        it "If doesn't find the task then return nil" do
+            @task = @todo_list.find_task_by_id(100)
+            expect(@task).to eq(nil)
+        end
+
+        it "Sorts list by its id's in ascending order" do
+            expect(@todo_list.sort_by_created("ASC")).to eq([@task1, @task2])    
+        end
+
+        it "Sorts list by its id's in descending order" do
+            expect(@todo_list.sort_by_created("DESC")).to eq([@task2, @task1])    
+        end
+
+        it "Checks for to do list users name" do
+            expect(@todo_list.user).to eq("Josh")
+        end
 
 
+
+
+
+
+
+
+        # it "Loads our to do list that we saved" do
+        #     @todo_list.save
+        #     save2 = TodoList.new("Josh")
+
+        #     save2.load_tasks
+
+        #     expect(save2.tasks.length).to eq(@todo_list.tasks.length)
+        # end
+    end
 end
