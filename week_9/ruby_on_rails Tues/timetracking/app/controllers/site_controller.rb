886932a25093b00a195008f284021399dd3bbd3d
Irene Fernandez <irenafnaf@gmail.com>
2016-06-21 20:40:52 -0400

    Added site controler and home and contact actions

---
+++ week_9/ruby_on_rails Tues/timetracking/app/controllers/site_controller.rb
@@ -0,0 +1,9 @@
+class SiteController < ApplicationController
+	def home #instance method for an action - in this case action being 'home'
+	end
+
+	def contact
+	end
+end
+
+
--- week_9/ruby_on_rails Tues/timetracking/config/routes.rb
+++ week_9/ruby_on_rails Tues/timetracking/config/routes.rb
@@ -1,4 +1,3 @@
-Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +52,21 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+Rails.application.routes.draw do
+  get '/' => 'site#home'
+  get '/contact' => 'site#contact'
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
