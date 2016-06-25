dd4f104fd724d330b6159b70b3a053e4e3182149
Irene Fernandez <irenafnaf@gmail.com>
2016-06-25 12:40:55 -0400

    Modified Project model and route.rb, added the projects controller and view as well

---
+++ week_9/ruby_on_rails Tues/timetracking/app/controllers/projects_controller.rb
@@ -0,0 +1,6 @@
+class ProjectsController < ApplicationController
+	def index
+		@projects = Project.limit(10).order(created_at: :desc)
+	end
+
+end
--- week_9/ruby_on_rails Tues/timetracking/app/models/project.rb
+++ week_9/ruby_on_rails Tues/timetracking/app/models/project.rb
@@ -1,2 +1,23 @@
 class Project < ActiveRecord::Base
+
+
+	def self.clean_old
+		where("created_at < ?", 1.week.ago).destroy_all
+	end
+
+	def self.last_created_projects(limit)
+		limit(limit).order(created_at: :desc)
+	end
 end
+
+
+# # EXAMPLE
+# class Project
+# 	attr_reader :name
+
+# 	def initialize(name)
+# 		@name = name
+# 	end 
+# end
+# p = Project.new("Ironhack")
+# p.name
