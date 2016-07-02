731f43c1ed1d81cf6cb6471f0f6e459a3bb638f7
Irene Fernandez <irenafnaf@gmail.com>
2016-07-02 12:05:41 -0400

    Created create and show method and  pages where the short link is generated and redirected to the original link

--- week_10/urlshortener/app/controllers/links_controller.rb
+++ week_10/urlshortener/app/controllers/links_controller.rb
@@ -1,4 +1,14 @@
 class LinksController < ApplicationController
+	def show
+		#input: params[:id]
+		@link = Link.find(params[:id])
+	end
+
+
+	def new
+		@link = Link.new
+	end 
+
 	def index 
 		params[:shortlink]
 		# input is short url in params[:shortlink]
@@ -11,5 +21,14 @@ class LinksController < ApplicationController
 		end
 	end
 
+	def create
+	# # input params[:link][:url]
+	link = Link.create(
+		url: params[:link][:url],
+		short_url: Link.generate_shortlink(3)
+	)
+
+	redirect_to "/links/#{link.id}"
+	end
 
 end
--- week_10/urlshortener/config/routes.rb
+++ week_10/urlshortener/config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+# root to: 'links#new'
+get '/' => 'links#new'
+get '/links/:id' => 'links#show'
+post '/links' => 'links#create', as: :links
+
                       # 'Controller#action'
                       # controller name# action'
-get '/' => 'links#home'
-
 get '/:shortlink' => 'links#index'
 end
