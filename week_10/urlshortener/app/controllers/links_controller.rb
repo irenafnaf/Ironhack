293061b19c137330c0f959127ff156ccc18b8deb
Irene Fernandez <irenafnaf@gmail.com>
2016-07-02 11:22:02 -0400

    Iteration 2 where we ccreated a short link generator in the link model

--- week_10/urlshortener/app/controllers/links_controller.rb
+++ week_10/urlshortener/app/controllers/links_controller.rb
@@ -1,9 +1,14 @@
 class LinksController < ApplicationController
 	def index 
 		params[:shortlink]
-		# input is short url params[:shortlink]
-		@link = Link.find_by(short_url: params[:shortlink])
-		redirect_to @link.url
+		# input is short url in params[:shortlink]
+		# 1. Find a link with a specific shorturl
+		link = Link.find_by(short_url: params[:shortlink])
+		
+		# outcome: redirect to original url
+		if link
+			redirect_to link.url
+		end
 	end
 
 
--- week_10/urlshortener/app/models/link.rb
+++ week_10/urlshortener/app/models/link.rb
@@ -1,2 +1,16 @@
 class Link < ActiveRecord::Base
+	
+	ALPHABET = [('a'..'z').to_a, ('A'..'Z').to_a].flatten
+	
+	def self.generate_shortlink(link_length)
+		# shortlink = ""
+		# link_length.times do 
+		# 	shortlink += ALPHABET[rand(ALPHABET.length)]
+		# end
+		# shortlink
+			
+		# end OR
+
+		ALPHABET.sample(link_length).join
+	end	
 end
