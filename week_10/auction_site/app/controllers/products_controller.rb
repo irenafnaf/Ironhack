a30e622ba6118f1f1e0d32a017bb1946be4ec86c
Irene Fernandez <irenafnaf@gmail.com>
2016-07-09 17:11:39 -0400

    Iteration 2-3 added bids

--- week_10/auction_site/app/controllers/products_controller.rb
+++ week_10/auction_site/app/controllers/products_controller.rb
@@ -4,6 +4,44 @@ class ProductsController < ApplicationController
 		@user = User.find(params[:user_id])
 		@product = @user.products.new
 	end
+
+	def all
+		@all_products = Product.all
+	end
+
+	def create
+        @user = User.find params[:user_id]
+
+
+        #permit is whitelisting only those attributes to be assigned
+        product_params = params.require(:product).permit(:title, :description, :deadline)
+
+        # without permit: @project.time_entries.new params[:time_entry]
+
+        @product = @user.products.new(product_params)
+        if @product.save
+            redirect_to user_products_path(@user.id)
+        else
+            render 'new'
+        end
+ 	end
+	
+ 	def show
+        @user = User.find params[:user_id]
+        @product = @user.products.find_by(id: params[:id])
+
+        @bids = @product.bids.order(amount: :desc)
+        @bid = Bid.new
+        @high_bid = @bids.first
+
+    end
+
+
+
+	def index
+		@user = User.find(params[:user_id])
+		@products = @user.products.all
+	end
 end
 
 
--- week_10/auction_site/config/routes.rb
+++ week_10/auction_site/config/routes.rb
@@ -4,5 +4,13 @@ get '/' => 'users#home'
 
     resources :users, only: [:new, :show, :create, :destroy] do 
       resources :products, only: [:index, :show, :create, :destroy, :new]
-  end
+    end
+
+
+get '/products' => 'products#all', as: :products
+
+
+post '/products/:product_id/bids' => 'bids#create', as: :product_bids
+
+
 end
