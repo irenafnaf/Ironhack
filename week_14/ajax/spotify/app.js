ea92f1b78d32c31af4bb6434a9bec7e0a914e735
Irene Fernandez <irenafnaf@gmail.com>
2016-07-30 11:53:09 -0400

    Iteration 2 of Spotify exercise

--- week_14/ajax/spotify/app.js
+++ week_14/ajax/spotify/app.js
@@ -1,8 +1,52 @@
 $(document).on("ready", function(){
+ 	
  	$("#js-form").on("submit", function(event){
  		event.preventDefault();
  		getArtists();
  	});
+
+
+	$("#js-artistlist").on("click", ".artist-li", function(event){
+		// console.log("Clicked");
+		// use data attribute to get artist id out if <li>
+		var artistId = $(event.currentTarget).data("artist-id");
+		// console.log(artistId);
+		var artistName = $(event.currentTarget).data("artist-name");
+		
+		$.ajax({
+			url: `https://api.spotify.com/v1/artists/${artistId}/albums`,
+			success: function(response){
+				showAlbums(response, artistName);
+			},
+			error: albumError,
+		})
+	})
+
+
+	function showAlbums(response, artistName){
+		console.log(response);
+		$(".modal-artist-name").text(artistName);
+		
+		var albums = response.items;
+		
+		albums.forEach(appendAlbum);
+		$(".modal").modal("show");
+	}
+
+	function appendAlbum (album){
+		 	var html = ` <li class="album-li" data-artist-id=${obj.id} data-album-name="${obj.name}">
+ 									<p>${obj.name}</p>
+
+ 								</li>
+						 `;
+			$(".modal-artist-albums").append(html);
+	}
+
+
+	function albumError(err){
+		console.log(err);
+	}
+
 
  	function getArtists(){
  		var searchTerm = $("#js-artist").val();
@@ -30,9 +74,12 @@ $(document).on("ready", function(){
  		artist.forEach(function(obj, idx){
  			appendArtist(obj);
  		})
+ 		$artistList.empty();
  	};
 
  	function appendArtist(obj){
+ 		var $artistList = $("#js-artistlist");
+
  		var artistImage;
 
  			 	if (obj.images[2]){
@@ -41,7 +88,7 @@ $(document).on("ready", function(){
  					artistImage = "http://placehold.it/200x200"
  				}
  			
- 			var html = ` <li>
+ 			var html = ` <li class="artist-li" data-artist-id=${obj.id} data-artist-name="${obj.name}">
  							<h3>${obj.name}</h3>
 
  							<img src=${artistImage}>
@@ -53,4 +100,3 @@ $(document).on("ready", function(){
 
 
  		
-});
